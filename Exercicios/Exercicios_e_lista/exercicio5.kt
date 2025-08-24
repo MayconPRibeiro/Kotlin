6fd724cfa1d1f833c77f8da4848b3a3646646219
MayconPRibeiro <maycon_ribeiro10@hotmail.com>
2025-08-24 12:45:34 -0300

    Atualizando com exercicio 5 pronto

---
+++ Exercicios/Exercicios_e_lista/exercicio5.kt
@@ -0,0 +1,12 @@
+package Exercicios_e_lista
+
+fun main(){
+    print("Nota 1: ")
+    val nota1 = readLine()!!.toDouble()
+    print("Nota 2: ")
+    val nota2 = readLine()!!.toDouble()
+
+    val nota_media = ((nota1 + nota2)/2)
+
+    println("A média entre $nota1 e $nota2 é $nota_media")
+}
---
+++ Exercicios/Exercicios_e_lista/exercicio6.kt
@@ -0,0 +1,2 @@
+package Exercicios_e_lista
+
